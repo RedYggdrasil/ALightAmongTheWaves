ae612463bdecf65b8896f98baab9ac9d97a6b395
RedYggdrasil <quentin.noguier@yahoo.fr>
2019-01-26 22:17:28 +0100

    Update Consequence enum

--- Assets/ALightAmongTheWaves/Scripts/Events/Consequence.cs
+++ Assets/ALightAmongTheWaves/Scripts/Events/Consequence.cs
@@ -4,17 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assets.ALightAmongTheWaves.Scripts.Events
+public enum Consequence
 {
-
-    public enum Consequence
-    {
-        NOTHING,
-        REDUCE_FOOD,
-        REDUCE_SURVIVORS,
-        REDUCE_WOOD,
-        INCREASE_FOOD,
-        INCREASE_SURVIVORS,
-        INCREASE_WOOD
-    }
+    NOTHING = 0,
+    REDUCE_FOOD = 1,
+    REDUCE_SURVIVORS = 2,
+    REDUCE_WOOD = 3,
+    INCREASE_FOOD = 4,
+    INCREASE_SURVIVORS = 5,
+    INCREASE_WOOD = 6
 }
