cc330b5f7ea180792be88ae26be5ff88b95ee7a1
DESKTOP-8NLM2N7\Portable DVC <roger.bordeaux@yahoo.fr>
2019-01-26 18:11:13 +0100

    GridSystem v0.9

--- Assets/ALightAmongTheWaves/Scripts/Ship/PlaceShipModule.cs
+++ Assets/ALightAmongTheWaves/Scripts/Ship/PlaceShipModule.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaceShipModule : MonoBehaviour
 {
     public GridSystem moduleGridSystem;
 
     public GameObject modulePrefab;
+
+    public GameObject modulePhantom;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +21,35 @@ public class PlaceShipModule : MonoBehaviour
     void Update()
     {
         Vector2 mouseInWorldSpace = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (moduleGridSystem.IsInBoat(mouseInWorldSpace))
+        {
+            if(modulePhantom == null)
+            {
+                modulePhantom = GameObject.Instantiate(modulePrefab, transform);
+                //modulePhantom.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
+            }
+
+            modulePhantom.transform.position = moduleGridSystem.GetPositionForCellIndex(moduleGridSystem.GetCellIndexFromPosition(mouseInWorldSpace));
+
+            if( moduleGridSystem.GetGameObjectFormCellIndex(moduleGridSystem.GetCellIndexFromPosition(mouseInWorldSpace)) != null)
+                modulePhantom.GetComponent<SpriteRenderer>().color = new Color(0.9f, 0.3f, 0.3f, 0.5f);
+            else
+                modulePhantom.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+
+        }
+        
                 
         if (Input.GetMouseButtonDown(0))
         {
-            if (moduleGridSystem.IsInBoat(mouseInWorldSpace))
-                moduleGridSystem.PutGameObjectOnPosition( GameObject.Instantiate(modulePrefab), mouseInWorldSpace);
+            if (moduleGridSystem.IsInBoat(mouseInWorldSpace) && 
+                moduleGridSystem.GetGameObjectFormCellIndex(moduleGridSystem.GetCellIndexFromPosition(mouseInWorldSpace)) == null )
+            {
+                modulePhantom.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+                moduleGridSystem.PutGameObjectOnPosition(modulePhantom, mouseInWorldSpace);
+                modulePhantom = null;
+            }
+                
 
         }
     }
