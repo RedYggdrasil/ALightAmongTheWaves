05beca215752e031315da32d662fd92e79fc13c4
RedYggdrasil <quentin.noguier@yahoo.fr>
2019-01-26 12:21:41 +0100

    Resources base scripts

---
+++ Assets/ALightAmongTheWaves/Scripts/StorageScripts/Resource.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType { Unset = 0, Food = 1, Wood = 2, Population = 3 }
+public static class ResourceTool
+{
+    static Dictionary<ResourceType, string> ResourceName = new Dictionary<ResourceType, string>
+    {
+        {ResourceType.Unset, "Unset"},
+        {ResourceType.Food, "Food"},
+        {ResourceType.Wood, "Wood"},
+        {ResourceType.Population, "Population"}
+    };
+}
+public interface IResource
+{
+    ResourceType ResourceType
+    {
+        get;
+    }
+    int MinAmount
+    {
+        get;
+        set;
+    }
+    int MaxAmount
+    {
+        get;
+        set;
+    }
+    int Amount
+    {
+        get;
+        set;
+    }
+}
+public abstract class AResource : IResource
+{
+    public abstract ResourceType ResourceType { get; }
+    public abstract int Amount { get; set; }
+    protected int _minAmount;
+    protected int _maxAmount;
+    public virtual int MinAmount { get { return _minAmount; } set { _minAmount = Mathf.Clamp(value, 0, int.MaxValue); EnforceAmountBoudaries(); } }
+    public virtual int MaxAmount { get { return _maxAmount; } set { _maxAmount = Mathf.Clamp(value, 0, int.MaxValue); EnforceAmountBoudaries(); } }
+
+    protected virtual void EnforceAmountBoudaries()
+    {
+        Amount = Mathf.Clamp(Amount, MinAmount, MaxAmount);
+    }
+}
+public abstract class IntBasedResource : AResource
+{
+    protected int _amount;
+    public override int Amount
+    {
+        get
+        {
+            return _amount;
+        }
+        set
+        {
+            _amount = value;
+        }
+    }
+
+}
+public abstract class ListBasedResource<T> : AResource where T : new()
+{
+    protected List<T> elements = new List<T>();
+
+    public override int Amount
+    {
+        get
+        {
+            return elements.Count;
+        }
+        set
+        {
+            value = Mathf.Clamp(value, MinAmount, MaxAmount);
+            if (elements.Count > value)
+            {
+                do
+                {
+                    elements.RemoveAt(Random.Range(0, elements.Count));
+                }
+                while (elements.Count > value);
+            }
+            else if (elements.Count < value)
+            {
+                do
+                {
+                    elements.Add( new T());
+                }
+
+                while (elements.Count > value);
+            }
+        }
+    }
+}
+public class Wood : IntBasedResource
+{
+    public override ResourceType ResourceType { get { return ResourceType.Wood; } }
+}
+public class Food : IntBasedResource
+{
+    public override ResourceType ResourceType { get { return ResourceType.Food; } }
+}
+public class Population : ListBasedResource<Human>
+{
+    public override ResourceType ResourceType { get { return ResourceType.Population; } }
+}
+public class Human
+{
+
+}
---
+++ Assets/ALightAmongTheWaves/Scripts/StorageScripts/Storage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Storage
+{
+    public static IResource[] resources;
+
+    public static Food food;
+    public static Wood wood;
+    public static Population population;
+
+    public static void Initialize()
+    {
+        resources = new IResource[3];
+        food = new Food();
+        resources[0] = food;
+        wood = new Wood();
+        resources[1] = wood;
+        population = new Population();
+        resources[2] = population;
+    }
+}
