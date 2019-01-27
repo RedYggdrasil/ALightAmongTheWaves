43b21c490e2193bb0673edfd892aba93f7af6d32
DESKTOP-8NLM2N7\Portable DVC <roger.bordeaux@yahoo.fr>
2019-01-27 03:46:02 +0100

    add UI to place part

---
+++ Assets/ALightAmongTheWaves/Scripts/UI/ShipPartSelector/ShipPartSelectorUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipPartSelectorUI : MonoBehaviour
+{
+    public Button menuButton;
+    public RectTransform panelPartTransform;
+    public Button partButtonPrefab;
+
+    public PlaceShipModule placeShipModule;
+    public ShipModule[] shipParts;
+
+    //public List<part>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(ShipModule shipModule in shipParts)
+        {
+            GameObject button = GameObject.Instantiate(partButtonPrefab.gameObject, panelPartTransform);
+            button.GetComponentInChildren<TextMeshProUGUI>().text = shipModule.name;
+            button.GetComponent<Button>().onClick.AddListener(() => {
+                placeShipModule.SetShipPartToPlace(GameObject.Instantiate(shipModule.gameObject).GetComponent<ShipModule>());
+                panelPartTransform.gameObject.SetActive(false);
+            });
+        }
+
+        menuButton.onClick.AddListener(() => panelPartTransform.gameObject.SetActive(!panelPartTransform.gameObject.activeSelf));
+    }
+
+}
